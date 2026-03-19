f424a76a31bb3d0884f3eb3bd771f865e3603cad
BHASVIC-EthanWilmot25 <ethan.wilmot25@students.bhasvic.ac.uk>
2026-03-19 15:44:03 +0000

    c'est incroyable

--- scrpit.js
+++ scrpit.js
@@ -2,3 +2,20 @@ function flibble(thing) {
 	div = document.getElementById(thing);
 	div.remove();
 }
+var sidebar = document.getElementById("hpome");
+
+function sigma() {
+	window.location.href = "the web if it was cool.html";
+}
+
+function sigma2() {
+	var turd = document.documentElement;
+		if(turd.requestFullscreen) {
+			turd.requestFullscreen();
+	}	
+}
+function s1() {
+	sidebar.style.backgroundImage=url("https://img.pixers.pics/pho_wat(s3:700/FO/57/91/04/63/700_FO57910463_40f1426f21ce28a77864f83931f0b682.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/wall-murals-fire-alphabet-number-1-one.jpg.jpg");
+}
+function s2() {
+	sidebar.
